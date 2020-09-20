218b9b26ab11c39d9dd1de7bd3b17b839844694a
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-09-20 09:12:46 +0300

    react class first commit

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+import Search from './Components/Search'
+import Table from './Components/Table'
+import Button from './Components/Button';
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+      searchKey:'',
+      searchTerm: DEFAULT_QUERY,
+      error:null,
+    };
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+   needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+}
+
+  setSearchTopStories (result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    const oldHits=results&&results[searchKey]
+      ? results[searchKey].hits
+      : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      results:
+      {
+        ...results,
+        [searchKey]: {hits: updatedHits, page}
+      }
+    });
+    }
+  fetchSearchTopStories (searchTerm, page=0) {
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this.setSearchTopStories(result.data))
+      .catch(error =>this.setState({error}));
+  }
+    componentDidMount() {
+      const { searchTerm } = this.state;
+      this.setState({ searchKey: searchTerm });
+      this.fetchSearchTopStories(searchTerm);
+  
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  onSearchSubmit (event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+  
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+}
+    event.preventDefault();
+  }
+
+  onDismiss (id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+    const isNotId = item => item.objectID !== id;
+
+    const updatedHits = hits.filter(isNotId);
+    this.setState({
+      results: {
+        ...results, 
+        [searchKey]:{ hits: updatedHits, page}
+      }
+    });
+  }
+
+  render () {
+    const { searchTerm, results,searchKey,error } = this.state;
+    const page = (results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+    
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+    if (error) {
+      return <p>Something went wrong.</p>;
+    }
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
+        {results &&
+          
+          <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
+        
+      </div>
+       
+    );
+  }
+}
+export default App;
---
+++ src/Components/Search.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+class Search extends Component {
+  render() {
+    const { value, onChange, onSubmit, children } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+       <input
+          type="text"
+          value={value}
+          onChange={onChange}
+            />
+            <button type="submit">
+                 {children} 
+            </button>
+      </form>
+    );
+  }
+}
+export default Search;
