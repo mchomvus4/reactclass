2fa41d5d007e4685e5130fbdb7fdacff1fb97992
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-10-06 08:43:18 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { sortBy } from 'lodash';
 import './App.css';
 import Search from './Components/Search'
 import Table from './Components/Table'
